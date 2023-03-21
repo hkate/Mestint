1da3077af69a8e08490d55eee6022b592ccd83be
Halász Kata <halaszk@mailbox.unideb.hu>
2023-03-21 13:33:14 +0100

    Update main02.py

--- Kommentelt_kiegészített/main02.py
+++ Kommentelt_kiegészített/main02.py
@@ -7,17 +7,15 @@ h = Hanoi(3)
 print(h.size, h.initial, h.goal)
 
 #Szélességi keresés futtatása
-breadth_first_tree_search(h).solution()
+print(breadth_first_tree_search(h).solution())
 
 #3 korsó szélességi kereséssel:
 from cup3 import Cup3
 
-"""
+
 # 3 korsó példányosítása
-c = Cup3((5,0,0), [(4,1,0)(4,0,1)],)
+c = Cup3((5,0,0), [(4,1,0), (4,0,1)])
 print(c.initial, c.goal)
 
 # Szélességi keresés futtatása
-breadth_first_tree_search(c).solution()
-"""
-#nem működik, nem tudom miért
+print(breadth_first_tree_search(c).solution())
